8c440dd83ab2ebeb858218273f2de5253661ac58
Omkar Garad <omkargarad6@gmail.com>
2023-01-23 12:44:37 +0530

    "Extent report"

--- src/test/java/NOV_Practice/pract02.java
+++ src/test/java/NOV_Practice/pract02.java
@@ -12,11 +12,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class pract02 {
+	
+	static ExtentReports report;
+	static ExtentTest test;
+	
+	
+	private String capture(WebDriver driver) throws Exception {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		File Dest = new File("src/../ErrImages/" + System.currentTimeMillis()
+		+ ".png");
+		String errflpath = Dest.getAbsolutePath();
+		FileUtils.copyFile(scrFile, Dest);
+		
+		//return null;
+		return errflpath;
+	}
+	
+	
+	@BeforeTest
+	public void startreport() {
+		report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html", true);
+		test = report.startTest("pract02");
+		
+	}
 
 	@Test
 		public void udemytest() throws Exception{
@@ -31,8 +61,20 @@ public class pract02 {
 			driver.get("https://www.amazon.in/ref=nav_logo");
 			//driver.manage().window().maximize();
 			
-
+			WebElement logo  = driver.findElement(By.id("nav-logo-sprites"));
+			
+			System.out.println("Height :" +logo.getRect().getDimension().getHeight());
+			
+			test.log(LogStatus.PASS, "Navigated to the specified URL");
+			test.log(LogStatus.PASS,test.addScreenCapture(capture(driver))+ "Test Pass");
 			
 
+	}
+	
+	@AfterTest
+	public static void endTest()
+	{
+	report.endTest(test);
+	report.flush();
 	}
 }
