9a6fc97bb43a9eeb4f1b5c00515790b214b9dd08
Omkar Garad <omkargarad6@gmail.com>
2023-01-31 14:46:43 +0530

    check

--- src/test/java/demoqa/Udemy_practice.java
+++ src/test/java/demoqa/Udemy_practice.java
@@ -235,4 +235,20 @@ public class Udemy_practice {
 		}
 	System.out.println("DONE");
 
+}
+	
+	public void relative_locators(){
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Shree/Desktop/jar/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		driver.manage().window().maximize();
+		
+		WebElement id = driver.findElement(By.id("username"));
+				id.sendKeys("rahulshettyacademy");
+				
+				WebElement pass = driver.findElement(By.id("password"));
+				pass.sendKeys("learning");
+
+
+
 }}
